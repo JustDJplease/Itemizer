ad3d165ce03b69b475c29bca03dbf31995fe826d
Floris <floris.jolink@gmail.com>
2018-10-16 21:26:23 +0200

    init commit

---
+++ src/main/java/me/nectarhoney/template/Template.java
@@ -0,0 +1,15 @@
+package me.nectarhoney.template;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Template extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
